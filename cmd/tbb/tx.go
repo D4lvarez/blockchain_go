9c1c74bd3afefd2fc4e477e3c5d15b5eaa01ecd0
Diego Alvarez <andresdaap2015@gmail.com>
2022-05-03 11:36:20 -0400

    Tx Command for CLI - add function

---
+++ cmd/tbb/tx.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/D4lvarez/blockchain_go/database"
+	"github.com/spf13/cobra"
+)
+
+const flagFrom string = "from"
+const flagTo string = "to"
+const flagValue string = "value"
+const flagData string = "data"
+
+func txCmd() *cobra.Command {
+	var txsCmd = &cobra.Command{
+		Use:   "tx",
+		Short: "Interact with transaction (add...)",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return incorrectUsageErr()
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+	txsCmd.AddCommand(txAddCmd())
+
+	return txsCmd
+}
+
+func txAddCmd() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "add",
+		Short: "Adds new transactions to database",
+		Run: func(cmd *cobra.Command, args []string) {
+			from, _ := cmd.Flags().GetString(flagFrom)
+			to, _ := cmd.Flags().GetString(flagTo)
+			value, _ := cmd.Flags().GetUint(flagValue)
+			data, _ := cmd.Flags().GetString(flagData)
+
+			fromAcc := database.NewAccount(from)
+			toAcc := database.NewAccount(to)
+
+			tx := database.NewTx(fromAcc, toAcc, value, data)
+
+			state, err := database.NewStateFromDisk()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			defer state.Close()
+
+			err = state.Add(tx)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			err = state.Persist()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			fmt.Println("TX successfully added to the ledger.")
+		},
+	}
+
+	cmd.Flags().String(flagFrom, "", "From what account to send tokens")
+	cmd.MarkFlagRequired(flagFrom)
+
+	cmd.Flags().String(flagTo, "", "To what account to send tokens")
+	cmd.MarkFlagRequired(flagTo)
+
+	cmd.Flags().Uint(flagValue, 0, "How many tokens to send")
+	cmd.MarkFlagRequired(flagValue)
+
+	cmd.Flags().String(flagData, "", "Possible values: 'reward'")
+
+	return cmd
+}
