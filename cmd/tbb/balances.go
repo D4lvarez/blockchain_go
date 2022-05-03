d560eb5927f90862fb75e010b4f4f66ff644f8bb
Diego Alvarez <andresdaap2015@gmail.com>
2022-05-03 11:36:35 -0400

    Balance Command for CLI - List function

---
+++ cmd/tbb/balances.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/D4lvarez/blockchain_go/database"
+	"github.com/spf13/cobra"
+)
+
+func balancesCmd() *cobra.Command {
+	var balancesCmd = &cobra.Command{
+		Use:   "balances",
+		Short: "Interact with balances (list...)",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return incorrectUsageErr()
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+
+	balancesCmd.AddCommand(balancesListCmd)
+
+	return balancesCmd
+}
+
+var balancesListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all balances",
+	Run: func(cmd *cobra.Command, args []string) {
+		state, err := database.NewStateFromDisk()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		defer state.Close()
+
+		fmt.Println("Account balances:")
+		fmt.Println("-----------------")
+
+		for account, balance := range state.Balances {
+			fmt.Println(fmt.Sprintf("%s: %d", account, balance))
+		}
+	},
+}
