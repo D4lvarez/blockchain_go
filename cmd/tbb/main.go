1d89faf23b2047b76d93484d06f1e0c407db422b
Diego Alvarez <andresdaap2015@gmail.com>
2022-05-04 13:17:30 -0400

    Add Run Command to CLI

--- cmd/tbb/main.go
+++ cmd/tbb/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	tbbCmd.AddCommand(versionCmd)
 	tbbCmd.AddCommand(balancesCmd())
-	tbbCmd.AddCommand(txCmd())
+	tbbCmd.AddCommand(runCmd())
 
 	err := tbbCmd.Execute()
 	if err != nil {
